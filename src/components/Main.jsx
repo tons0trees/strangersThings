21a42cd5811d08c60e8dc24b18ff958a2ba7c6a7
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-18 09:35:14 -0500

    setup fetch function, made new PageDisplay component.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,24 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import {Navbar} from "./"
 
 const Main = () => {
+const [postList, setPostList] = useState([])
+
+async function getPostList() {
+const response = await fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-FT/posts')
+const posts = await response.json()
+// const posts2 = posts.data.posts
+setPostList(posts.data.posts)
+}
+
+useEffect (() => {
+    getPostList()
+}, [])
+
+
+
+getPostList()
+
     return (
         <div id="main">
             <Navbar />
---
+++ src/components/PostsDisplay.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+
+const PostsDisplay = () => {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default PostsDisplay
